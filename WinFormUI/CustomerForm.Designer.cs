52846a2e55fabb708047d56f1798de80ace4d3f2
HuynhTrungTin52 <524k0033@student.tdtu.edu.vn>
2026-05-02 05:17:02 +0700

    Add project files.

---
+++ WinFormUI/CustomerForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WinFormUI
+{
+    partial class CustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.btnAddCustomer = new System.Windows.Forms.Button();
+            this.btnGetCustomer = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CustomerDataView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerDataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CustomerID";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(153, 120);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(153, 153);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(153, 189);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(100, 22);
+            this.txtCustomerID.TabIndex = 5;
+            // 
+            // btnAddCustomer
+            // 
+            this.btnAddCustomer.Location = new System.Drawing.Point(132, 341);
+            this.btnAddCustomer.Name = "btnAddCustomer";
+            this.btnAddCustomer.Size = new System.Drawing.Size(75, 23);
+            this.btnAddCustomer.TabIndex = 6;
+            this.btnAddCustomer.Text = "Add customer";
+            this.btnAddCustomer.UseVisualStyleBackColor = true;
+            this.btnAddCustomer.Click += new System.EventHandler(this.btnAddCustomer_Click);
+            // 
+            // btnGetCustomer
+            // 
+            this.btnGetCustomer.Location = new System.Drawing.Point(462, 341);
+            this.btnGetCustomer.Name = "btnGetCustomer";
+            this.btnGetCustomer.Size = new System.Drawing.Size(75, 23);
+            this.btnGetCustomer.TabIndex = 7;
+            this.btnGetCustomer.Text = "Get customer";
+            this.btnGetCustomer.UseVisualStyleBackColor = true;
+            this.btnGetCustomer.Click += new System.EventHandler(this.btnGetCustomer_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(194, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Developed by Huỳnh Trung Tín";
+            // 
+            // CustomerDataView
+            // 
+            this.CustomerDataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomerDataView.Location = new System.Drawing.Point(334, 78);
+            this.CustomerDataView.Name = "CustomerDataView";
+            this.CustomerDataView.RowHeadersWidth = 51;
+            this.CustomerDataView.RowTemplate.Height = 24;
+            this.CustomerDataView.Size = new System.Drawing.Size(343, 214);
+            this.CustomerDataView.TabIndex = 9;
+            // 
+            // CustomerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CustomerDataView);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnGetCustomer);
+            this.Controls.Add(this.btnAddCustomer);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomerForm";
+            this.Text = "CustomerForm";
+            this.Load += new System.EventHandler(this.CustomerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerDataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.Button btnAddCustomer;
+        private System.Windows.Forms.Button btnGetCustomer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView CustomerDataView;
+    }
+}
+
---
+++ WinFormUI/CustomerForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BLL; //using BusinessLogicLayer;
+using DAL; //using DataAccessLayer;
+
+
+namespace WinFormUI
+{
+
+    public partial class CustomerForm : Form
+    {
+        private readonly CustomerService _customerService;
+        public CustomerForm()
+        {
+            InitializeComponent();
+            _customerService = new CustomerService();
+        }
+
+
+        private void btnAddCustomer_Click(object sender,
+EventArgs e)
+        {
+            var customer = new Customer
+            {
+                Name = txtName.Text,
+                Email = txtEmail.Text
+            };
+            _customerService.AddCustomer(customer);
+            LoadCustomers();
+            MessageBox.Show("Customer added successfully!");
+        }
+
+        private void btnGetCustomer_Click(object sender,
+EventArgs e)
+        {
+            int customerId = int.Parse(txtCustomerID.Text);
+            var customer =
+            _customerService.GetCustomer(customerId);
+            if (customer != null)
+            {
+                txtName.Text = customer.Name;
+                txtEmail.Text = customer.Email;
+            }
+            else
+            {
+                MessageBox.Show("Customer not found!");
+            }
+        }
+
+        private void CustomerForm_Load(object sender, EventArgs e)
+        {
+            LoadCustomers();
+        }
+        private void LoadCustomers()
+        {
+            var customers = _customerService.GetAllCustomers();
+            CustomerDataView.DataSource = customers;
+        }
+    }
+}
